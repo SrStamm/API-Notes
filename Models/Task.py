a31cc6d959ec56075f3eb9a4b7f951cee1a646b1
Mirko Alexander <mirkostamm19@gmail.com>
2025-01-31 17:50:25 +0000

    crear_actualizar_task

--- Models/Task.py
+++ Models/Task.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
 
 class Task(BaseModel): 
-    id : int
     texto : str
--- routers/task.py
+++ routers/task.py
@@ -21,54 +21,37 @@ def tasks_all():
 def tasks_search_id(id: int):
     with Session(engine) as session:
         statement = select(Tasks).where(Tasks.id == id)
-        results = session.exec(statement)
+        results = session.exec(statement).first()
         return results
 
 # Crea una nueva tarea
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_task(task : Task):
-    # Verificamos que no exista una tarea con el mismo id
-    for index, task_search in enumerate(task_list):
-        if task_search.id == task.id:
-            raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail={"Ya existe una tarea con este id"})
-    
-    task_list.append(task)
-    return {"Se creo una nueva tarea."}
-
-
-
-
-# Crea una nueva tarea
-@router.post("/bd", status_code=status.HTTP_201_CREATED)
-def create_task(task : Task):
-    db = Session()
-    new_task = Tasks(**Task.dict())
-    db.add(new_task)
-    db.commit()
-    return Task.dict()
-
-
-
-
-
-
-
-
+def create_task(task : Tasks):
+    with Session(engine) as session:
+        session.add(task)
+        session.commit()
+        return {"Se creo una nueva tarea."}
 
 # Actualiza un usuario segun su ID
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_task(task: Task):
-    found = False   # Indica si se encontro el usuario
+def update_task(task: Tasks):
+    with Session(engine) as session:
+        statement = select(Tasks).where(Tasks.id == task.id)
+        result = session.exec(statement)
+        
+        # Obtiene un unico valor
+        task_selected = result.one()
+        
+        # Se modifica el registro
+        task_selected.text = task.text
+        
+        # Se guardan los cambios
+        session.commit()
+        
+        return {"Ha sido actualizado con exito"}
 
-    for index, task_search in enumerate(task_list):
-        if task_search.id == task.id:
-            task_list[index] = task
-            found = True
-    
-    if not found:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"No se ha encontrado la tarea"})
-    else: 
-        return {"La tarea se ha actualizado"}
+
+
 
 
 # Elimina la tarea con id especifico
